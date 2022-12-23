66175208cc6789ead697318ec364cadeaf6cf933
Elay Gelbart <elaygelbart@gmail.com>
2022-12-23 19:47:14 +0200

    google.com init sveletekit

--- google/src/routes/+page.svelte
+++ google/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+    <span>Gmail</span>
+    <span>Images</span>
+    <span id="google-apps">Image in the futere</span>
+    <span id="google-account">Google account here in the futere</span>
+</header>
+
+<main>
+    <div id="google-logo"><img src="/google.png" alt="google-logo"/></div>
+    <div id="input-bar"><input type="text"/></div>
+    <div id="search-btns"></div>
+    <div id="language-offers">Google offered in: <a dir="rtl" href="https://www.google.com/setprefs?sig=0_WCA61GKU5MMIPYl0OZNfeKUBN70%3D&hl=iw&source=homepage&sa=X&ved=0ahUKEwjWzrKtmZD8AhXPDewKHcQnAEMQ2ZgBCBM">עברית</a> <a dir="rtl" href="https://www.google.com/setprefs?sig=0_WCA61GKU5MMIPYl0OZNfeKUBN70%3D&hl=ar&source=homepage&sa=X&ved=0ahUKEwjWzrKtmZD8AhXPDewKHcQnAEMQ2ZgBCBQ">العربية</a> </div>
+</main>
+
+
+<footer>
+    <div id="footer-language"></div>
+    <div id="about"></div>
+</footer>
