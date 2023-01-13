b9dc21feb478bd98b75063ef45125f3827b7f5c5
Elay Gelbart <elaygelbart@gmail.com>
2023-01-13 13:05:55 +0200

    google final basic hd design

--- google/src/routes/+page.svelte
+++ google/src/routes/+page.svelte
@@ -1,9 +1,4 @@
 <style>
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
     header {    
         display: flex;
         align-items: center;
@@ -27,7 +22,11 @@
         gap: 1rem;
     }
     .header-selection-texts{
-        color: #5f6368;
+        color: #000000;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        
     }
 
     .header-selection-images{
@@ -35,11 +34,42 @@
         align-items: center;
         gap: 1rem;
     }
+    .sign-in-btn{
+        cursor: pointer;
+        color: #ffffff;
+        text-decoration: none;
+        background: #1b66c9;
+        box-shadow: 0 1px 3px 1px rgb(66 64 67 / 15%), 0 1px 2px 0 rgb(60 64 67 / 30%);
+        margin-left: 8px;
+        border: 1px solid transparent;
+        font-weight: 500;
+        font-size: 14px;
+        letter-spacing: 0.25px;
+        line-height: 16px;
+        margin-left: 10px;
+        margin-right: 8px;
+        min-width: 96px;
+        padding: 9px 23px;
+        text-align: center;
+        vertical-align: middle;
+        -webkit-border-radius: 4px;
+        border-radius: 4px;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        outline: none;
+        display: inline-block;
+    }
+
+    main{
+        height: calc(100% - 300px);
+        width: 100%;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
     #google-logo {
-        min-height: 92px;
-        max-height: 290px;
-        height: calc(100% - 560px);
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -54,6 +84,7 @@
         max-height: 160px;
         padding: 20px;
         margin: 0 auto;
+        margin-top: 60px;
         padding-top: 6px;
         width: auto;
         max-width: 584px;
@@ -68,10 +99,9 @@
         border: 1px solid #dfe1e5;
         box-shadow: none;
         border-radius: 24px;
-        margin: 0 auto;
-        width: 100%;
+        width: 180%;
         max-width: 584px;
-        padding: 5px 8px 0 14px;
+        padding: 12px 18px 10px 12px;
     }
     #input-bar input {
     height: 100%;
@@ -90,6 +120,34 @@
         display: flex;
         align-items: center;
         gap: 1rem;
+    }
+    #search-btns {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        margin-top: 20px;
+        height: 70px;
+    }
+    #search-btns button {
+        background-color: #f8f9fa;
+        border: 1px solid #f8f9fa;
+        border-radius: 4px;
+        color: #3c4043;
+        font-family: arial,sans-serif;
+        font-size: 14px;
+        margin: 11px 4px;
+        padding: 0 16px;
+        line-height: 27px;
+        height: 36px;
+        min-width: 54px;
+        text-align: center;
+        cursor: pointer;
+        user-select: none;
+    }
+    #language-offers a{
+        color: #1a0dab;
+        text-decoration: none;
     }
     footer {
         position: absolute;
@@ -138,30 +196,31 @@
         </div>
         <div class="header-selection-images">
             <svelte:component this={GoogleAppsSvg} />
-            <img src="/googleNameTag.png" alt="Google Name Tag"/>
+            <a class="sign-in-btn" href="https://accounts.google.com/ServiceLogin?hl=en&amp;passive=true&amp;continue=https://www.google.com/&amp;ec=GAZAmgQ" target="_top">Sign in</a>
         </div>
     </div>
 </header>
 
 <main>
-    <div id="google-logo"><img src="/google.png" alt="google-logo"/></div>
+    <div>
+        <div id="google-logo"><img src="/google.png" alt="google-logo"/></div>
 
-    <form action="/search" method="GET" role="search">
-        <div id="input-bar">
-            <svelte:component this={GoogleSearchSvg} />
-            <input type="text"/>
-            <div id="input-bar-right-container">
-                <svelte:component this={GoogleMicrophoneSvg} />
-                <svelte:component this={GoogleCameraSvg} />
+        <form action="/search" method="GET" role="search">
+            <div id="input-bar">
+                <svelte:component this={GoogleSearchSvg} />
+                <input type="text"/>
+                <div id="input-bar-right-container">
+                    <svelte:component this={GoogleMicrophoneSvg} />
+                    <svelte:component this={GoogleCameraSvg} />
+                </div>
             </div>
-        </div>
-        <div id="search-btns">
-            <button type="submit">Google Search</button>
-            <button type="button">I'm Feeling Lucky</button>
-        </div>
-        <div id="language-offers">Google offered in: <a dir="rtl" href="https://www.google.com/setprefs?sig=0_WCA61GKU5MMIPYl0OZNfeKUBN70%3D&hl=iw&source=homepage&sa=X&ved=0ahUKEwjWzrKtmZD8AhXPDewKHcQnAEMQ2ZgBCBM">עברית</a> <a dir="rtl" href="https://www.google.com/setprefs?sig=0_WCA61GKU5MMIPYl0OZNfeKUBN70%3D&hl=ar&source=homepage&sa=X&ved=0ahUKEwjWzrKtmZD8AhXPDewKHcQnAEMQ2ZgBCBQ">العربية</a> </div>
-    </form>
-
+            <div id="search-btns">
+                <button type="submit">Google Search</button>
+                <button type="button">I'm Feeling Lucky</button>
+            </div>
+            <div id="language-offers">Google offered in: <a dir="rtl" href="https://www.google.com/setprefs?sig=0_WCA61GKU5MMIPYl0OZNfeKUBN70%3D&hl=iw&source=homepage&sa=X&ved=0ahUKEwjWzrKtmZD8AhXPDewKHcQnAEMQ2ZgBCBM">עברית</a> <a dir="rtl" href="https://www.google.com/setprefs?sig=0_WCA61GKU5MMIPYl0OZNfeKUBN70%3D&hl=ar&source=homepage&sa=X&ved=0ahUKEwjWzrKtmZD8AhXPDewKHcQnAEMQ2ZgBCBQ">العربية</a> </div>
+        </form>
+    </div>
 </main>
 
 
